52d0a659a2d6d38afe039b40bafd4911ee19a45d
zabulus <zabulus@apriorit.com>
2014-05-08 14:38:05 +0300

    Make vc110 build

--- packaging/nuget/build32.bat
+++ packaging/nuget/build32.bat
@@ -1,7 +1,7 @@
 REM This script assumes we are running
 REM from the OpenSLL repo root.
 
-CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+CALL "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
 
 perl Configure VC-WIN32 %1 %2 %3 %4 %5 %6 %7 %8 %9
 CALL ms\do_ms.bat
